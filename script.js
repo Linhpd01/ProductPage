9943730db542b0e8059cc57d1b642ac67e9cd0f1
Duy Linh Pham <99628680+Linhpd01@users.noreply.github.com>
2022-04-25 00:57:41 +0700

    Website ProductPage
    Create Website ProductPage

---
+++ script.js
@@ -0,0 +1,10 @@
+$('.related-product').slick({
+    slidesToShow: 4,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows:true,
+    prevArrow: false,
+    nextArrow: false,
+    dots: true,
+  });
